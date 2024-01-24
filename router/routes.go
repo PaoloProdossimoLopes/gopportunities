8ce3113f2832f13224f3f9fe4d8d45f8296638a6
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-01-24 20:40:11 -0300

    ✨ add initial routing for opening opportunities

--- router/routes.go
+++ router/routes.go
@@ -9,9 +9,39 @@ import (
 func attachRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1/")
 
-	v1.GET("/opening", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "GET Opening",
-		})
+	v1.GET("/openings", showOpeningHandler)
+	v1.GET("/openings/:openingId", showSpecificOpening)
+	v1.POST("/openings", createOpeningHander)
+	v1.DELETE("/openings", deleteOpeningHander)
+	v1.PUT("/openings", updateOpeningHander)
+}
+
+func showOpeningHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	})
+}
+
+func showSpecificOpening(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "GET Specific Opening",
+	})
+}
+
+func createOpeningHander(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "POST Opening",
+	})
+}
+
+func deleteOpeningHander(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "DELETE Opening",
+	})
+}
+
+func updateOpeningHander(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "UPDATE Opening",
 	})
 }
