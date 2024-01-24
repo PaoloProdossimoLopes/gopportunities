23e45fc8fd5243081900f6d3a25286933cbaadc6
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-01-24 20:00:02 -0300

    ✨ initialize route with gin gonic

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", get)
+
+	router.Run()
+}
+
+func get(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
