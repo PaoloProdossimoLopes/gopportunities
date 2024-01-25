63912ebd7982dbcdde28a01743c107e255d9ac0c
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-01-25 05:46:45 -0300

    ✨ add `OpeningResponse` struct

--- schemas/openings.go
+++ schemas/openings.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -13,4 +15,16 @@ type Opening struct {
 	Remote   bool
 	Link     string
 	Salary   int64
+}
+
+type OpeningResponse struct {
+	Id        uint      `json:"id"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
